e15e79886831e8f8010ea0d1573a7121bcacbde2
santoshlearner07 <nandiyawarsantosh.0719@gmail.com>
2021-12-24 08:39:49 +0530

    Added: NewCode practice

--- defaultParameter.js
+++ defaultParameter.js
@@ -3,3 +3,9 @@ function myFunction(x, y=10 ) {
 }
 // myFunction(5);
 console.log("The value -> " + myFunction(5))
+
+
+const urFunc= (name,age=20) => {
+    return `Hi ${name} Your age is ${age} years old`
+}
+console.log(urFunc('Ross',16)); //If you remove 16 it will take deafult value 20
--- destructuringAssignment.js
+++ destructuringAssignment.js
@@ -11,3 +11,15 @@ let myName, myRole;
 let object = {myName:'John',myRole:'SoftWare'};
 ({myName,myRole} = object)
 console.log(myName,myRole)
+
+const contacts = {
+    name:'John',
+    familyName:'Grey',
+    age: 20
+}
+let {name,familyName,age} = contacts;
+console.log(name)
+console.log(familyName)
+console.log(age)
+console.log(`${name} ${familyName} ${age}`)
+console.log("Details -> "+contacts)
